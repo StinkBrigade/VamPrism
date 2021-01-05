370a9e5462e9a58ea9a3b72a8fbd3e94a23f9ab2
SparkleKimera <kai.anderson.ellis.brown@gmail.com>
2021-01-05 08:46:06 +0000

    Cleaned Up ClientUtil
    Will write a new C U B E   M E T H O D.... Uh, eventually.

--- src/main/java/kimera/really/works/vamprism/client/util/ClientUtil.java
+++ src/main/java/kimera/really/works/vamprism/client/util/ClientUtil.java
@@ -2,34 +2,24 @@ package kimera.really.works.vamprism.client.util;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
+import kimera.really.works.vamprism.common.blocks.BlockRegistry;
+import kimera.really.works.vamprism.common.tileentity.AbstractPrismaStorerTileEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.vector.Matrix3f;
 import net.minecraft.util.math.vector.Matrix4f;
+import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.world.World;
+import net.minecraftforge.client.model.data.EmptyModelData;
 
 public final class ClientUtil
 {
-    public static final void drawCube(IVertexBuilder vertexBuilder, MatrixStack matrixStack, float x, float y, float z, float width, float height, float depth, float xUV1, float xUV2, float yUV1, float yUV2, int red, int blue, int green, int alpha)
-    {
-        matrixStack.push();
-
-        MatrixStack.Entry matrixStackEntry = matrixStack.getLast();
-        Matrix4f matrix4f = matrixStackEntry.getMatrix();
-        Matrix3f matrix3f = matrixStackEntry.getNormal();
-
-        float xOffset = width / 2.0F;
-        float yOffset = height / 2.0F;
-        float zOffset = depth / 2.0F;
-
-        drawQuad(vertexBuilder, matrix4f, matrix3f, x - xOffset, x + xOffset, y - yOffset, y + yOffset, z - zOffset, 0, false, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
-        drawQuad(vertexBuilder, matrix4f, matrix3f, x - xOffset, x + xOffset, y - yOffset, y + yOffset, z + zOffset, 0, true, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
-        drawQuad(vertexBuilder, matrix4f, matrix3f, z - zOffset, z + zOffset, y - yOffset, y + yOffset, x - xOffset, 1, false, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
-        drawQuad(vertexBuilder, matrix4f, matrix3f, z - zOffset, z + zOffset, y - yOffset, y + yOffset, x + xOffset, 1, true, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
-        drawQuad(vertexBuilder, matrix4f, matrix3f, x - xOffset, x + xOffset, z - zOffset, z + zOffset, y - yOffset, 2, false, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
-        drawQuad(vertexBuilder, matrix4f, matrix3f, x - xOffset, x + xOffset, z - zOffset, z + zOffset, y + yOffset, 2, true, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
-
-        matrixStack.pop();
-    }
-
     public static final void drawBeam(IVertexBuilder vertexBuilder, MatrixStack matrixStack, float x, float y, float z, float width, float depth, int segments, float segmentHeight, int currentFrame, int totalFrames, int red, int blue, int green, int alpha, boolean fade)
     {
         MatrixStack.Entry matrixStackEntry = matrixStack.getLast();
@@ -71,38 +61,13 @@ public final class ClientUtil
         float front = z - zOffset;
         float back = z + zOffset;
 
-        drawVertQuad(vertexBuilder, matrix4f, matrix3f, left, right, y, top, front, front, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
-        drawVertQuad(vertexBuilder, matrix4f, matrix3f, right, left, y, top, back, back, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
-        drawVertQuad(vertexBuilder, matrix4f, matrix3f, left, left, y, top, front, back, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
-        drawVertQuad(vertexBuilder, matrix4f, matrix3f, right, right, y, top, back, front, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
+        drawQuad(vertexBuilder, matrix4f, matrix3f, left, right, y, top, front, front, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
+        drawQuad(vertexBuilder, matrix4f, matrix3f, right, left, y, top, back, back, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
+        drawQuad(vertexBuilder, matrix4f, matrix3f, left, left, y, top, front, back, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
+        drawQuad(vertexBuilder, matrix4f, matrix3f, right, right, y, top, back, front, xUV1, xUV2, yUV1, yUV2, red, blue, green, alpha);
     }
 
-    public static final void drawQuad(IVertexBuilder vertexBuilder, Matrix4f matrix4f, Matrix3f matrix3f, float a1, float a2, float b1, float b2, float c, int axis, boolean flipped, float xUV1, float xUV2, float yUV1, float yUV2, int red, int blue, int green, int alpha)
-    {
-        float x1 = axis == 1 ? c : axis == 0 && flipped ? a1 : a2;
-        float x2 = axis == 1 ? c : axis == 0 && flipped ? a2 : a1;
-        float y1 = axis == 2 ? c : axis == 1 && flipped ? b2 : b1;
-        float y2 = axis == 2 ? c : axis == 1 && flipped ? b1 : b2;
-        float z1 = axis == 0 ? c : axis == 2 ? flipped ? b1 : b2 : a1;
-        float z2 = axis == 0 ? c : axis == 2 ? flipped ? b2 : b1 : a2;
-        float z3 = axis == 2 ? z1 : z2;
-        float z4 = axis == 2 ? z2 : z1;
-
-        drawVertex(vertexBuilder, matrix4f, matrix3f, x2, y1, z3, xUV2, yUV1, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
-        drawVertex(vertexBuilder, matrix4f, matrix3f, x2, y2, z2, xUV2, yUV2, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
-        drawVertex(vertexBuilder, matrix4f, matrix3f, x1, y2, z4, xUV1, yUV2, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
-        drawVertex(vertexBuilder, matrix4f, matrix3f, x1, y1, z1, xUV1, yUV1, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
-    }
-
-    public static final void drawHorizQuad(IVertexBuilder vertexBuilder, Matrix4f matrix4f, Matrix3f matrix3f, float x1, float x2, float z1, float z2, float y, float xUV1, float xUV2, float yUV1, float yUV2, int red, int blue, int green, int alpha)
-    {
-        drawVertex(vertexBuilder, matrix4f, matrix3f, x2, y, z1, xUV2, yUV1, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
-        drawVertex(vertexBuilder, matrix4f, matrix3f, x2, y, z2, xUV2, yUV2, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
-        drawVertex(vertexBuilder, matrix4f, matrix3f, x1, y, z2, xUV1, yUV2, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
-        drawVertex(vertexBuilder, matrix4f, matrix3f, x1, y, z1, xUV1, yUV1, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
-    }
-
-    public static final void drawVertQuad(IVertexBuilder vertexBuilder, Matrix4f matrix4f, Matrix3f matrix3f, float x1, float x2, float y1, float y2, float z1, float z2, float xUV1, float xUV2, float yUV1, float yUV2, int red, int blue, int green, int alpha)
+    public static final void drawQuad(IVertexBuilder vertexBuilder, Matrix4f matrix4f, Matrix3f matrix3f, float x1, float x2, float y1, float y2, float z1, float z2, float xUV1, float xUV2, float yUV1, float yUV2, int red, int blue, int green, int alpha)
     {
         drawVertex(vertexBuilder, matrix4f, matrix3f, x2, y1, z2, xUV2, yUV2, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
         drawVertex(vertexBuilder, matrix4f, matrix3f, x2, y2, z2, xUV2, yUV1, red, blue, green, alpha, 0.0F, 1.0F, 0.0F);
@@ -113,5 +78,36 @@ public final class ClientUtil
     public static final void drawVertex(IVertexBuilder vertexBuilder, Matrix4f matrix4f, Matrix3f matrix3f, float x, float y, float z, float xUV, float yUV, int red, int green, int blue, int alpha, float normalX, float normalY, float normalZ)
     {
         vertexBuilder.pos(matrix4f, x, y, z).color(red, green, blue, alpha).tex(xUV, yUV).overlay(OverlayTexture.NO_OVERLAY).lightmap(15728880).normal(matrix3f, 0.0F, 1.0F, 0.0F).endVertex();
+    }
+
+    public static final void renderSunlightCube(AbstractPrismaStorerTileEntity tileEntity, double x, double startY, double endY, double z, float width, float height, float depth, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn)
+    {
+        BlockPos blockPos = tileEntity.getPos();
+        BlockState blockState = BlockRegistry.SUNLIGHT_BLOCK.get().getDefaultState();
+
+        float scalePercentage = tileEntity.getTotalFillRatio();
+        scalePercentage = MathHelper.clamp(scalePercentage, 0.001F, 1F);
+
+        matrixStack.push();
+        matrixStack.translate(x, (scalePercentage * (endY - startY)) + startY, z);
+        matrixStack.rotate(Vector3f.YP.rotationDegrees(0.0F));
+        matrixStack.scale(width, scalePercentage * height, depth);
+
+        BlockRendererDispatcher blockRendererDispatcher = Minecraft.getInstance().getBlockRendererDispatcher();
+        World world = tileEntity.getWorld();
+        blockRendererDispatcher.getBlockModelRenderer().renderModel(
+                world,
+                blockRendererDispatcher.getModelForState(blockState),
+                blockState,
+                blockPos,
+                matrixStack,
+                bufferIn.getBuffer(RenderTypeLookup.func_239221_b_(blockState)),
+                false,
+                world.rand,
+                blockState.getPositionRandom(blockPos),
+                OverlayTexture.NO_OVERLAY,
+                EmptyModelData.INSTANCE);
+
+        matrixStack.pop();
     }
 }
